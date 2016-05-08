6b8a53807f1c924f411aa721c680c0e52f9358ab
Kris De Volder <kdevolder@pivotal.io>
2016-05-07 18:09:33 -0700

    Some tweaks to test listener

--- src/test/java/com/github/kdvolder/minireactor/tests/TestBracketer.java
+++ src/test/java/com/github/kdvolder/minireactor/tests/TestBracketer.java
@@ -1,33 +1,56 @@
 package com.github.kdvolder.minireactor.tests;
 
+import org.testng.ITestClass;
+import org.testng.ITestNGListener;
 import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
+import org.testng.annotations.Listeners;
 
 public class TestBracketer extends TestListenerAdapter {
-	
+
+	private boolean isEnabled(ITestResult result) {
+		Class<?> klass = result.getMethod().getTestClass().getRealClass();
+		Listeners listeners = klass.getAnnotation(Listeners.class);
+		if (listeners!=null) {
+			for (Class<? extends ITestNGListener> l : listeners.value()) {
+				if (l==TestBracketer.class) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 	@Override
 	public void onTestStart(ITestResult result) {
 		super.onTestStart(result);
-		ITestNGMethod m = result.getMethod();
-		String name = m.getMethodName();
-		System.out.println(">>> "+name);
+		if (isEnabled(result)) {
+			ITestNGMethod m = result.getMethod();
+			String name = m.getMethodName();
+			System.out.println(">>> "+name);
+		}
 	}
 	
 	@Override
 	public void onTestFailure(ITestResult result) {
 		super.onTestFailure(result);
-		ITestNGMethod m = result.getMethod();
-		String name = m.getMethodName();
-		System.out.println("<<< "+name+": FAILED");
+		if (isEnabled(result)) {
+			ITestNGMethod m = result.getMethod();
+			String name = m.getMethodName();
+			System.out.println("<<< "+name+": FAILED");
+		}
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
 		super.onTestSuccess(result);
-		ITestNGMethod m = result.getMethod();
-		String name = m.getMethodName();
-		System.out.println("<<< "+name+": success!");
+		if (isEnabled(result)) {
+			ITestNGMethod m = result.getMethod();
+			String name = m.getMethodName();
+			System.out.println("<<< "+name+": success!");
+		}
 	}
 
 }
+
