90df58b32c66137415605f603ff078b97f2665cf
Kris De Volder <kdevolder@pivotal.io>
2016-05-07 17:30:14 -0700

    Add another test

--- src/test/java/com/github/kdvolder/minireactor/tests/FluxTest.java
+++ src/test/java/com/github/kdvolder/minireactor/tests/FluxTest.java
@@ -7,10 +7,13 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.Random;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
@@ -21,55 +24,20 @@ import com.google.common.io.CharStreams;
 public class FluxTest {
 
 	/**
-	 * When this is set to true, then a 'verify_output' which fails to find the
-	 * the expected output resource to compare with will save the current output
-	 * (so it can be compared with on the next test-run).
-	 */
-	private static final boolean RECORD_OUTPUT = true;
-	
-	/**
-	 * When set to true the output sent via 'println' is also shown on System.out.
-	 * If set to false it is only recorded silently for comparison to the expected
-	 * output.
-	 */
-	private static final boolean SHOW_OUTPUT = true;
-
-	/**
 	 * A fixed seed so tests involving randomness are predictable.
 	 */
 	protected static final long SEED = 678; // make tests involving random, predictable.
 	
-	private StringBuilder out = new StringBuilder();
+	private OutputVerifier out;
 	
-	private void println(Object msg) {
-		if (SHOW_OUTPUT) {
-			System.out.println("%out: "+msg);
-		}
-		out.append(msg+"\n");
+	@BeforeMethod
+	public void setupOutputCapture(Method m) {
+		out = new OutputVerifier(m.getName());
+		out.RECORD_OUTPUT = true;
 	}
 	
-	private void verify_output(String testKey) throws Exception {
-		String actualOutput = out.toString();
-		String testFileName = testKey+".out.txt";
-		try {
-			InputStream is = this.getClass().getResourceAsStream(testFileName);
-			assertNotNull(is, "resource at 'src/test/resources/"+testFileName+"'");
-			String expectedOutput = CharStreams.toString(new InputStreamReader(is, "utf8"));
-			assertEquals(expectedOutput, actualOutput);
-		} catch (AssertionError e) {
-			if (!RECORD_OUTPUT) {
-				throw e;
-			}
-			URL myLocation = this.getClass().getResource(this.getClass().getSimpleName()+".class");
-			File myFile = new File(myLocation.toURI());
-			assertTrue(myFile.toString().contains("/target/test-classes/"));
-			File resourcesDir = new File(myFile.getParentFile().toString().replace("/target/test-classes/", "/src/test/resources/"));
-			resourcesDir.mkdirs();
-			File dataFile = new File(resourcesDir, testFileName);
-			try (OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(dataFile), "utf8")) {
-				out.write(actualOutput);
-			}
-		}
+	private void println(Object m) {
+		out.println(m);
 	}
 	
 	@Test
@@ -99,34 +67,37 @@ public class FluxTest {
 				println("!!!COMPLETE!!!");
 			}
 		});
-		verify_output("infinite_cache");
+		out.verify();
 	}
 
-	void main5() {
+	@Test
+	public void hot_stream() throws Exception {
 		Flux<Integer> diceRolls = diceRoller();
 
-		System.out.println("Rolling the dice:");
+		println("Rolling the dice:");
 		diceRolls
 		.take(5)
-		.consume(System.out::println);
+		.consume(this::println);
 		
-		System.out.println("Rolling the dice again:");
+		println("Rolling the dice again:");
 		diceRolls
 		.take(5)
 		.subscribe(new BasicSubscriber<Integer>() {
 			@Override
 			public void onNext(Integer t) {
-				System.out.println(t);
+				println(t);
 			}
 			@Override
 			public void onError(Throwable t) {
-				System.out.println("!!!ERROR: "+t.getMessage());
+				println("!!!ERROR: "+t.getMessage());
 			}
 			@Override
 			public void onComplete() {
-				System.out.println("===============");
+				println("!!!COMPLETE!!!");
 			}
 		});
+		
+		out.verify();
 	}
 
 	/**
@@ -137,8 +108,8 @@ public class FluxTest {
 	 * observe a different sequence of values.
 	 */
 	private Flux<Integer> diceRoller() {
+		Random generator = new Random(SEED);
 		return Flux.fromIterable(() -> new Iterator<Integer>() {
-			Random generator = new Random(SEED);
 
 			@Override
 			public boolean hasNext() {
@@ -188,10 +159,5 @@ public class FluxTest {
 		num.consume((i) -> System.out.println(i));
 	}
 
-
-	@Test
-	public void test() {
-		
-	}
 	
 }
---
+++ src/test/java/com/github/kdvolder/minireactor/tests/OutputVerifier.java
@@ -0,0 +1,86 @@
+package com.github.kdvolder.minireactor.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+
+import com.google.common.io.CharStreams;
+
+/**
+ * Meant to be used by tests that are expected to produce 
+ * the same deterministic, printed output on every test run.
+ * <p>
+ * It provides the means to record and save the output to
+ * a file, as well as compare recorded output to a previously
+ * recorded output.
+ * 
+ * @author Kris De Volder
+ */
+public class OutputVerifier {
+	
+	private StringBuilder out = new StringBuilder();
+	
+	private String testKey;
+	
+	public OutputVerifier(String testKey) {
+		this.testKey = testKey;
+	}
+	
+	/**
+	 * When this is set to true, then a 'verify_output' which fails to find the
+	 * the expected output resource to compare with will save the current output
+	 * (so it can be compared with on the next test-run).
+	 * <p>
+	 * Use with care! This meant to help create 'expected_output' files easily.
+	 * But you do not want to leave it on all the time and risk to have tests
+	 * accidentally passing when they are missing the expected output file.
+	 */
+	public boolean RECORD_OUTPUT = false;
+	
+	/**
+	 * When set to true the output sent via 'println' is also shown on System.out.
+	 * If set to false it is only recorded silently for comparison to the expected
+	 * output.
+	 */
+	public boolean SHOW_OUTPUT = true;
+
+	/**
+	 * Print some recorded output. 
+	 */
+	public void println(Object msg) {
+		if (SHOW_OUTPUT) {
+			System.out.println("%out: "+msg);
+		}
+		out.append(msg+"\n");
+	}
+	
+	public void verify() throws Exception {
+		String actualOutput = out.toString();
+		String testFileName = testKey+".out.txt";
+		InputStream is = this.getClass().getResourceAsStream(testFileName);
+		if (is!=null) {
+			String expectedOutput = CharStreams.toString(new InputStreamReader(is, "utf8"));
+			assertEquals(expectedOutput, actualOutput);
+		} else {
+			assertTrue(RECORD_OUTPUT);
+			URL myLocation = this.getClass().getResource(this.getClass().getSimpleName()+".class");
+			File myFile = new File(myLocation.toURI());
+			assertTrue(myFile.toString().contains("/target/test-classes/"));
+			File resourcesDir = new File(myFile.getParentFile().toString().replace("/target/test-classes/", "/src/test/resources/"));
+			resourcesDir.mkdirs();
+			File dataFile = new File(resourcesDir, testFileName);
+			try (OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(dataFile), "utf8")) {
+				out.write(actualOutput);
+			}
+		}
+	}
+
+
+
+}
